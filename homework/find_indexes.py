dda0fa7959ce99c4097ab9ef732c142d518b5f9f
A1sh1teru <reserved.coliseum@gmail.com>
2023-09-08 20:19:17 +0300

    done find_indexes

--- homework/find_indexes.py
+++ homework/find_indexes.py
@@ -1,18 +1,11 @@
-def find_indexes(arr):
-    indexes = [1, 3, 5, 8, 10, 12]
-    for i in range(1, len(arr)):
-        if arr[i] - arr[i-1] > 1:
-            indexes.append(i)
-    if len(indexes) == 0:
+def find_index(arr):
+    index_list = [i for i in range(1, len(arr)) if arr[i] - arr[i - 1] > 1]
+    if not index_list:
         return "Не найдено"
-    elif len(indexes) == 1:
-        return indexes[0]
+    elif len(index_list) == 1:
+        return index_list[0]
     else:
-        return indexes
-
-# array = [1, 3, 5, 8, 10, 12]
-# result = find_indexes(array)
-# print(result)
-
-# def find_indexes(x):
-#     array = [1, 4, 5, 6, 9, 14, 18, 19, 20, 22, 25, 26]
+        return index_list
+    
+arr = [-19, -18, -17, -16,  -10, 0, 20]
+print(find_index(arr))
