c37c76faa7a3e5063036bf7adee28468bb33b1f8
A1sh1teru <reserved.coliseum@gmail.com>
2023-09-09 12:24:58 +0300

    updated program simple_guess

--- practice_3/simple_guess.py
+++ practice_3/simple_guess.py
@@ -1,11 +1,28 @@
 #вводим число, если корень целый, то оставляем, если нет, то появляется комментарий "вывести трудно"
 
-def guess(x: int) -> int | str:
-    x: int = int(input('Введите натуральное число: '))
-    for n in range(x+1):
-        if n*n == x: 
-            return n
+# def guess(x: int) -> int | str:
+#     x: int = int(input('Введите натуральное число: '))
+#     for n in range(x+1):
+#         if n*n == x: 
+#             return n
+#         if n*n > x:
+#             break
     
-    return "Вывести трудно"
+#     return "Вывести трудно"
 
-print(guess(25))
+# print(guess(x))
+
+def guess(x: int) -> int | str:
+    if x == 0:
+        return 0
+    
+    for n in range(1, x+1):
+        if n*n == x:
+            return n
+        if n*n > x:
+            break
+            
+    return "Слишком сложно, не могу вывести результат"
+
+x = int(input('Введите натуральное число: '))
+print(guess(x))
