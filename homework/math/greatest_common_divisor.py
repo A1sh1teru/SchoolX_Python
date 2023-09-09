bc43ec8eafa46e422fa36a7ee634411b75a7f63f
A1sh1teru <reserved.coliseum@gmail.com>
2023-09-10 00:36:09 +0300

    added the folder 'math' and done the greatest_common_divisor

---
+++ homework/math/greatest_common_divisor.py
@@ -0,0 +1,17 @@
+def max_common_div(a: int, b: int) -> int:
+
+    if not (a > 0 and b > 0):
+        return 0
+
+    while a > 0 and b > 0:
+        if a > b:
+            a = a - b
+        else:
+            b = b - a
+
+    return max(a, b)
+
+a = int(input('Введите значение для натурального числа "a": '))
+b = int(input('Введите значение для натурального числа "b": '))
+
+print(max_common_div(a, b))
